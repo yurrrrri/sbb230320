7e29ac852dc84f8cd590cf8d7752dc852f2656c2
yurrrrri <yu_ri_son@naver.com>
2023-03-20 14:55:18 +0900

    질문 답변 엔티티

---
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -0,0 +1,23 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    private Integer questionId;
+}
